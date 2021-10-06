06312fc8c4ad35fc4f303dd4fd469931c2ecd017
Landon Yarrington <jly.dev.36963@gmail.com>
2021-10-06 14:56:03 -0600

    reorganize

--- go/pg-example/providers/providers.go
+++
@@ -1,8 +0,0 @@
-package providers
-
-import "go-pg-practice/dal/pgdal"
-
-// Providers -- context/dependencies
-type Providers struct {
-	PGDAL pgdal.IPGDAL
-}
--- ui/vue-nuxt-jsx-express-base/api/index.js
+++
@@ -1,8 +0,0 @@
-// imports
-const express = require('express')
-// app instance
-const app = express()
-// use api routes
-app.use('/hello', require('./routes/api/hello'))
-// export app
-module.exports = app
