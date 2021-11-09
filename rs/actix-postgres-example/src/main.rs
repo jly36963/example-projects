40c93228aabbfd4ca239e0a6aec3806ae8ec5cf1
Landon Yarrington <jly.dev.36963@gmail.com>
2021-11-09 06:29:14 -0700

    rs actix tokio-postgres

--- rs/actix-postgres-example/src/main.rs
+++ rs/actix-postgres-example/src/main.rs
@@ -8,7 +8,7 @@ use env_logger::Env;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Logger
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Server
     let providers = providers::setup_providers().await;
     api::start(providers).await
